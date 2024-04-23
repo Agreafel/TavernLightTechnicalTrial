787c0901d1912df6a2de5c715098b251c93bdb3d
agreafel <25335635+Agreafel@users.noreply.github.com>
2024-04-22 19:07:20 -0700

    Q7 Comments

--- bouncing_button/bouncing_button.lua
+++ bouncing_button/bouncing_button.lua
@@ -2,8 +2,6 @@ local bouncing_buttonButton = nil
 
 local bouncing_buttonWindow = nil
 
-local console_color =  {r = 255, g = 0, b = 255, a = 255}
-
 local marginForSpawn = 70
 
 local shift_event
@@ -33,6 +31,7 @@ end
 function hide()
     bouncing_buttonWindow:setVisible(false)
     bouncing_buttonButton:setOn(false)
+    -- end the button shift cycle
     if shift_event then
         shift_event:cancel()
         shift_event = nil
@@ -42,6 +41,7 @@ end
 function show()
     bouncing_buttonWindow:setVisible(true)
     bouncing_buttonButton:setOn(true)
+    -- start the button shift event cycle
     shift_event = scheduleEvent(
         function() 
             shift_button_left(bouncing_buttonWindow:getChildById("Jump"),
@@ -51,9 +51,12 @@ function show()
         end, shift_delay)
 end
 
+-- A function that is used as a periodic event to shift the button
 function shift_button_left(jumpButton, window, speed, delay)
     if window and window:isVisible() then
         local windowPosition = window:getPosition()
+        -- an assistive function that maintains the bounds of the button within the window
+        -- and loops to the opposite side when the button reaches past the bounds
         local function loopingMovement(currentX)
             local winWidth = window:getWidth()
             if currentX < windowPosition.x+jumpButton:getMarginLeft() then
@@ -62,27 +65,37 @@ function shift_button_left(jumpButton, window, speed, delay)
             return currentX
         end
         local jumpButtonPosition = jumpButton:getPosition()
+        -- shifting the button by speed for each interval of delay
         jumpButton:move(loopingMovement(jumpButtonPosition.x - speed), jumpButtonPosition.y)
+        -- Continue the event cycle
         shift_event = scheduleEvent(function() shift_button_left(jumpButton, window, speed, delay) end, delay)
         return
     end
+    -- Clear the event cycle when the window is not visible
     if shift_event then
         shift_event:cancel()
         shift_event = nil
     end
 end
 
+-- The behavior function when the button is clicked
 function bouncing_button()
+    -- Need the window to be open, sanity check
     if bouncing_buttonButton:isOn() then
+        -- Get all of the data we need to operate
         local jumpButton = bouncing_buttonWindow:getChildById("Jump")
         local windowPosition = bouncing_buttonWindow:getPosition()
+        -- Create the random position (within the bounds of the window)
         local randomPosition = { x = windowPosition.x+bouncing_buttonWindow:getWidth()-jumpButton:getMarginLeft(),
                                 y = (math.random(windowPosition.y+jumpButton:getMarginRight(),
                                 windowPosition.y+bouncing_buttonWindow:getHeight()-marginForSpawn)) }
+        -- Place the button at the decided location
         jumpButton:setPosition(randomPosition)
     end
 end
 
+
+-- Need to destroy the elements
 function terminate()
     bouncing_buttonButton:destroy()
     bouncing_buttonWindow:destroy()
